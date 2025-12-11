3ca0a0bb45326012c61169b5a6915683258fc245
Kritchadakorn <kritchadakorn.do@rmuti.ac.th>
2025-12-11 22:45:56 +0700

    Update GenerateArray.java

--- Lab#2/GenerateArray.java
+++ Lab#2/GenerateArray.java
@@ -15,9 +15,12 @@ public class GenerateArray {
 
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Input n : ");
         int n = sc.nextInt();
+        System.out.print("Input i : ");
         int i = sc.nextInt();
         int[] result = gen(n, i);
         System.out.print("Gen(" + n + "," + i + ") = " + Arrays.toString(result));
     }
 }
+
